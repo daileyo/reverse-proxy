7bee6965f1cfecb72bd03c2886d948226140d59c
Robert Dailey <dale13@gmail.com>
2021-03-24 08:09:22 -0700

    Whitespace cleanup

--- start-httpd.sh
+++ start-httpd.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-
 rm -rf /run/httpd/* /tmp/httpd*
-
-
 #copy conf files to apache before start... force it if needed.
 /bin/cp -af /from-host/. /etc/httpd/conf.d/
-
 exec /usr/sbin/httpd -D FOREGROUND
