96a42688034e072ef5210ac5108c4234ef1c6541
Robert Dailey <dale13@gmail.com>
2021-03-30 15:42:39 -0700

    Added reverse proxy configurations Added default landing page for reverse proxy server Updated docker startup script.

--- start-httpd.sh
+++ start-httpd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf /run/httpd/* /tmp/httpd*
 #copy conf files to apache before start... force it if needed.
-/bin/cp -af /from-host/. /etc/httpd/conf.d/
+#/bin/cp -af /from-host/. /etc/httpd/conf.d/
+#/bin/mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.bk
 exec /usr/sbin/httpd -D FOREGROUND
